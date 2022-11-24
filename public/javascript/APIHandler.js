2d705d2cab945285989bada58bef1a06f9415e5e
Guilhem Martin Saint Léon <guilhem.msl@gmail.com>
2022-11-24 15:43:21 +0100

    not finished, stuck on iteration 3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,28 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl,
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api.get("/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister(characterId) {
+    return this.api.get(`/characters/${characterId}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister(data) {
+    return this.api.post(`/characters`, data);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(characterId, data) {
+    return this.api.put(`/characters/${characterId}`, data);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    return this.api.delete(`/characters/${characterId}`);
   }
 }
